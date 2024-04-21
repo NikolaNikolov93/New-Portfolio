d7ee1c23d24fafc0af3b5ee6816ff8d52eb0262b
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2024-04-21 13:39:18 +0300

    Added typewritter effect to homescreen

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,23 @@
 import styles from "./Home.module.css";
+import Typewriter from "typewriter-effect";
 const Home = () => {
   return (
-    <div className={styles["home"]}>
-      <img src="../../home.jpg" alt="" />
+    <div
+      style={{ backgroundImage: `url(../../home.jpg)` }}
+      className={styles["home"]}
+    >
+      <p>I'm</p>
+      <Typewriter
+        options={{
+          strings: [
+            "a web developer!",
+            "an enthusiast!",
+            "a hobby photographer!",
+          ],
+          autoStart: true,
+          loop: true,
+        }}
+      />
     </div>
   );
 };
