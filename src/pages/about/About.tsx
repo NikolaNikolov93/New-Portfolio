257aeee7118fee3bef871d9ca1471882aa7acdaa
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2024-04-22 22:51:36 +0300

    Added About page and css fix for Home page

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,8 +1,50 @@
+import styles from "./About.module.css";
 const About = () => {
   return (
-    <div>
-      <h1>This is about</h1>
-    </div>
+    <>
+      <section className={styles["about"]}>
+        <h1>About</h1>
+        <p>
+          I was born and grew up in Pazardzhik city, Bulgaria. I went to school
+          in "Konstantin Velichkov" High school of Mathematics that is located
+          in my hometown. I love playing football and going in the nature. There
+          aren't many places that I haven't been in Bulgaria. I also like the
+          adrenaline rush experiences. I have been to multiple mountain hikes,
+          bungee jump and etc. I love playing video games even though I can't
+          find time to play them that much anymore.
+        </p>
+        <div className={styles["detailed-info"]}>
+          <div
+            className={styles["detailed-info-img"]}
+            style={{ backgroundImage: `url(../../cvp2.jpg)` }}
+          ></div>
+          <div className={styles["my-info"]}>
+            <h2>JavaScript/React Developer</h2>
+            <p>I studied JavaScript and React at SoftUni</p>
+            <ul>
+              <li className={styles["ul-flex-item"]}>
+                Brithday: 28 October 1993
+              </li>
+              <li className={styles["ul-flex-item"]}>Website: example.com</li>
+              <li className={styles["ul-flex-item"]}>Phone: +359890303047</li>
+              <li className={styles["ul-flex-item"]}>Country: Bulgaria</li>
+              <li className={styles["ul-flex-item"]}>City: Pazardzhik</li>
+              <li className={styles["ul-flex-item"]}>Age: 30</li>
+              <li className={styles["ul-flex-item"]}>Degree: High school</li>
+              <li className={styles["ul-flex-item"]}>
+                Email: niksnikolov93@gmail.com
+              </li>
+            </ul>
+            <p>
+              I'm a hard working, honest individual. I'm always willing to learn
+              new skills. I'm friendly, helpful and polite. I'm able to work
+              independently in busy environments and also within a team setting.
+              I'm able to listen effectively when solving problems.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
